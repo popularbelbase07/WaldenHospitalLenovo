67d4b4bbc4e9984dbbe9f76238a8252a5852057c
ganga ammay <gang0029@edu.easj.dk>
2019-05-02 11:20:40 +0200

    Registration page updation

--- WaldenHospitalLenovo/ViewPage/HomePage.xaml.cs
+++ WaldenHospitalLenovo/ViewPage/HomePage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WaldenHospitalLenovo.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,11 +21,23 @@ namespace WaldenHospitalLenovo.ViewPage
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class HomePage : Page
     {
-        public BlankPage1()
+        public HomePage()
         {
             this.InitializeComponent();
+        }
+
+       
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
