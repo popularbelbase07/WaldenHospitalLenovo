14cc5b1aa06a29d67f70538c7a27ea1f065cc3ed
Popular Belbase <popu0003@edu.easj.dk>
2019-05-03 20:52:54 +0200

    Adding some classes..

---
+++ WaldenHospitalLenovo/Event Handler/NotifyPropertyChanged.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WaldenHospitalLenovo.Annotations;
+
+namespace WaldenHospitalLenovo.Event_Handler
+{
+    public class NotifyPropertyChanged :INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    
+}
--- WaldenHospitalLenovo/Model/Login.cs
+++ WaldenHospitalLenovo/Model/Login.cs
@@ -13,12 +13,11 @@ namespace WaldenHospitalLenovo.Model
         public string UserName { get; set; }
 
         public string Password { get; set; }
-        
-        //Constructor
 
+        //Constructor
         public Login()
         {
-            
+
         }
 
         public Login(string userName, string password)
