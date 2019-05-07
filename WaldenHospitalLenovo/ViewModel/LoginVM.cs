f724552be8c1e564f137a089b18f7037af2e4bc3
Popular Belbase <popu0003@edu.easj.dk>
2019-05-07 16:42:15 +0200

    delete comments

--- WaldenHospitalLenovo/ViewModel/LoginVM.cs
+++ WaldenHospitalLenovo/ViewModel/LoginVM.cs
@@ -14,33 +14,18 @@ namespace WaldenHospitalLenovo.ViewModel
     public class LoginVm
     {
         public List<Login> ListLogin { get; set; }
-        //public Login Authentication { get; set; }
+       
        
         public LoginVm()
         {
             ListLogin = GetLoginFromDb();
-            //Authentication=new Login();
+          
 
         }
         public async void LoginPage(string userName , string password)
         {
             foreach (var login in ListLogin)
             {
-                //if ((login.UserName==Authentication.UserName) && (login.Password==Authentication.Password))
-                //{
-                //    Type homeType = typeof(HomePage);
-                //    FrameNavigate.ActivateFrameNavigation(homeType);
-                //    var success = new MessageDialog("Welcome to homepage");
-                //    await success.ShowAsync();
-                //    break;
-                //}
-                //else
-                //{
-                //    var failure = new MessageDialog("Given input is not matched !!");
-                //    await failure.ShowAsync();
-                //    break;
-                //}
-
                 if ((login.UserName == userName) && (login.Password == password))
                 {
                     //page navigation
@@ -62,14 +47,14 @@ namespace WaldenHospitalLenovo.ViewModel
             }
 
         }
-// Catalogue
+            // Catalogue
         public List<Login> GetLoginFromDb()
         {
             return new List<Login>()
             {
                 new Login("User1" , "Khem"),
                 new Login("User2" , "Popular"),
-                new Login("User3" , "Ganga"),
+                new Login("User3" , "Ganga")
 
             };
         }
