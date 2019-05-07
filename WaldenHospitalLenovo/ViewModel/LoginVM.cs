bef5e4f36367759321b6f58c7038c7e1462acddb
khempant <khempant311@gmail.com>
2019-05-07 12:56:44 +0200

    ddd

--- WaldenHospitalLenovo/ViewModel/LoginVM.cs
+++ WaldenHospitalLenovo/ViewModel/LoginVM.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WaldenHospitalLenovo.Model;
 
 namespace WaldenHospitalLenovo.ViewModel
 {
-    class LoginVM
+     public class LoginVm
     {
-    }
+        public List<Login> ListLogin { get; set; }
+        public Login CheckAuthentication { get; set; }
+
+        public LoginVm()
+        {
+            CheckAuthentication = new Login();
+            ListLogin = GetLoginFromDb();
+        }
 }
