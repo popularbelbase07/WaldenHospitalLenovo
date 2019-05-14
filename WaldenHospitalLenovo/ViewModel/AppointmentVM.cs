1fc0d5282fa966cc0e052b24277a0e1aeb35ec8a
Popular Belbase <popu0003@edu.easj.dk>
2019-05-14 22:54:22 +0200

    jpt

--- WaldenHospitalLenovo/ViewModel/AppointmentVM.cs
+++ WaldenHospitalLenovo/ViewModel/AppointmentVM.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WaldenHospitalLenovo.Event_Handler;
+using WaldenHospitalLenovo.Common;
 using WaldenHospitalLenovo.Model;
 
 namespace WaldenHospitalLenovo.ViewModel
