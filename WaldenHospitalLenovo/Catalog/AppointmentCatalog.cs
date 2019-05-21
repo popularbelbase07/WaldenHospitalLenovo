74a5bda447ca24c4ce879bacf68ebca760e515bb
Popular Belbase <popu0003@edu.easj.dk>
2019-05-21 19:29:32 +0200

    change

--- WaldenHospitalLenovo/Catalog/AppointmentCatalog.cs
+++ WaldenHospitalLenovo/Catalog/AppointmentCatalog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +10,40 @@ namespace WaldenHospitalLenovo.Catalog
 {
    public  class AppointmentCatalog
    {
-       private List<Appointment> myAppointments;
+       //private List<Appointment> myAppointments;
+
+       //public AppointmentCatalog()
+       //{
+       //    myAppointments= new List<Appointment>();
+       //    Appointment ap= new Appointment();
+       //    // we are here
+       //}
+
+       ////public void addAppointment( , , , , , )
+       ////{
+       ////     Appointment ap= new Appointment(, , , , , );
+       //    myAppointments.Add(ap);
+       //}
+       private ObservableCollection<Appointment> myAppointments;
+       public ObservableCollection<Appointment> GetAppointment
+       {
+           get { return myAppointments;}
+           set { myAppointments = value; }
+       }
+
+       private static AppointmentCatalog _instance;
+
+       public static AppointmentCatalog Instance => _instance ?? (new AppointmentCatalog());
 
        public AppointmentCatalog()
        {
-           myAppointments= new List<Appointment>();
-           Appointment ap= new Appointment();
-           // we are here
+         myAppointments=new ObservableCollection<Appointment>()
+         {
+             //new Appointment(1,2,(DateTime.Now,DateTime.Now.Hour ,new DateTime(2019/05/22)))
+           
+         };
        }
 
-       //public void addAppointment( , , , , , )
-       //{
-       //     Appointment ap= new Appointment(, , , , , );
-       //    myAppointments.Add(ap);
-       //}
 
    }
 }
--- WaldenHospitalLenovo/Model/Appointment.cs
+++ WaldenHospitalLenovo/Model/Appointment.cs
@@ -3,23 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WaldenHospitalLenovo.Common;
 
 namespace WaldenHospitalLenovo.Model
 {
-  public  class Appointment
+  public  class Appointment :NotifyPropertyChanged
   {
       private Patient p;
       private Doctor d;
       private DateTime _from;
       private DateTime _to;
       private DateTime calender;
+      public Patient P
+      {
+          get { return p;}
+          set
+          {
+              p = value;
+              OnPropertyChanged(nameof(p));
+          }
 
-        public Appointment() { }
+      }
+      public Doctor D
+      {
+          get { return d; }
+          set
+          {
+              d = value;
+              OnPropertyChanged(nameof(D));
+          }
+      }
+      public DateTime TimeFrom
+      {
+          get { return _from; }
+          set
+          {
+              _from = value;
+              OnPropertyChanged(nameof(TimeFrom));
+          }
+      }
+      public DateTime TimeTo
+      {
+          get { return _to; }
+          set
+          {
+              _to = value;
+              OnPropertyChanged(nameof(TimeTo));
+          }
+      }
 
-      public Appointment(Patient p , Doctor d,  DateTime  from, DateTime to , DateTime calender )
+        public Appointment()
       {
 
+      }
 
+      //public Appointment(Patient p , Doctor d,  DateTime  from, DateTime to , DateTime calender )
+      //{
+
+
+      //}
+      public Appointment(Patient p, Doctor d, DateTime @from, DateTime to, DateTime calender)
+      {
+          this.p = p;
+          this.d = d;
+          _from = @from;
+          _to = to;
+          this.calender = calender;
       }
 
   }
